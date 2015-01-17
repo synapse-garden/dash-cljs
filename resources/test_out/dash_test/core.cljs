d7ba700ce45e4591251c9a967d4e79a9918b38ec
Bodie Solomon <binary132@gmail.com>
2015-01-17 15:28:08 -0500

    Removed spurious files from out directories and revised gitignore

--- resources/test_out/dash_test/core.cljs
+++
@@ -1,8 +0,0 @@
-(ns dash-test.core)
-
-(defn insert-ids [maps]
-  "insert-ids takes a sequence of maps and updates each element with an :id
-   from sequential integers >= 1."
-  (map (fn [m i] (assoc m :id i))
-    maps
-    (iterate inc 1)))
